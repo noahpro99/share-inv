0db43bfbc1f12bcd6c241f4b16ed4f073cac5714
noahpro <noahpro@gmail.com>
2025-08-09 19:08:30 -0400

    builds

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Minecraft Fabric mod development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openjdk
+            gradle
+            git
+          ];
+          shellHook = ''
+            echo "Welcome to the Minecraft Fabric mod dev shell!"
+            echo "Java: $(java -version 2>&1 | head -n 1)"
+            echo "Gradle: $(gradle --version | head -n 1)"
+            echo "You may want to run 'gradle genSources' or 'gradle build'."
+          '';
+        };
+      }
+    );
+}
